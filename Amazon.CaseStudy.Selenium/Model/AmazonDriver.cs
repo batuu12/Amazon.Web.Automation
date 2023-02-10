ead14ded0dbfab72177634732eb7de1964836482
batuu12 <batuhancanci22@gmail.com>
2023-02-10 23:23:02 +0300

    Project has been created

---
+++ Amazon.CaseStudy.Selenium/Model/AmazonDriver.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Amazon.CaseStudy.Selenium.Model
+{
+	public class AmazonDriver
+	{
+        private static readonly AmazonDriver instance = new AmazonDriver();
+
+        private readonly IWebDriver webDriver;
+        private readonly WebDriverWait waitDriver;
+        private static string amazonWindowHandle;
+
+        static AmazonDriver()
+        {
+        }
+
+        private AmazonDriver()
+        {
+            webDriver = new ChromeDriver();
+
+            waitDriver = new WebDriverWait(webDriver, TimeSpan.FromSeconds(30));
+
+            webDriver.Manage().Window.Maximize();
+            webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            webDriver.Url = UrlRepository.amazonUrl;
+
+            amazonWindowHandle = webDriver.CurrentWindowHandle;
+        }
+
+        public static IWebDriver WebDriver
+        {
+            get
+            {
+                instance.webDriver.SwitchTo().Window(amazonWindowHandle);
+                return instance.webDriver;
+            }
+        }
+
+        public static WebDriverWait Wait
+        {
+            get
+            {
+                return instance.waitDriver;
+            }
+        }
+
+    }
+}
+
---
+++ Amazon.CaseStudy.Selenium/Model/UrlRepository.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Amazon.CaseStudy.Selenium.Model
+{
+	public class UrlRepository
+	{
+		public static string amazonUrl = "https://www.amazon.com.tr";
+	}
+}
+
